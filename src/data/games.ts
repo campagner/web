f83d6f6c259add9349b6f829aa3ab920c5e9287a
Alexandre Campagner Carvalho <campagnercarvalho@gmail.com>
2025-05-04 19:37:47 -0300

    Alterado caminho das imagens

--- src/data/games.ts
+++ src/data/games.ts
@@ -7,7 +7,7 @@ export const games: Game[] = [
     title: 'Prince of Persia',
     year: '1989',
     description: 'Um jogo de plataforma cinematográfico onde você navega por armadilhas mortais e duelos de espada para salvar a princesa em 60 minutos.',
-    imageUrl: 'src/assets/images/prince.png',
+    imageUrl: 'images/prince.png',
     archiveUrl: 'https://archive.org/details/msdos_Prince_of_Persia_1990',
   },
   {
@@ -15,7 +15,7 @@ export const games: Game[] = [
     title: 'Stunts',
     year: '1990',
     description: 'Um avançado jogo de corrida 3D com editor de pistas e recursos para manobras como loopings, saltos e parafusos.',
-    imageUrl: 'src/assets/images/stunts.png',
+    imageUrl: 'images/stunts.png',
     archiveUrl: 'https://archive.org/details/msdos_Stunts_1990',
   },
   {
@@ -23,7 +23,7 @@ export const games: Game[] = [
     title: 'Test Drive',
     year: '1987',
     description: 'Corra com carros esportivos exóticos em estradas perigosas à beira de precipícios, evitando o tráfego e a polícia.',
-    imageUrl: 'src/assets/images/td.png',
+    imageUrl: 'images/td.png',
     archiveUrl: 'https://archive.org/details/msdos_Test_Drive_1987',
   },
   {
@@ -31,7 +31,7 @@ export const games: Game[] = [
     title: 'Golden Axe',
     year: '1990',
     description: 'Jogo de arcade hack and slash com deslocamento lateral com três heróis lutando para derrotar o maligno Death Adder.',
-    imageUrl: 'src/assets/images/golden.png',
+    imageUrl: 'images/golden.png',
     archiveUrl: 'https://archive.org/details/msdos_Golden_Axe_1990',
   },
   {
@@ -39,7 +39,7 @@ export const games: Game[] = [
     title: 'Doom',
     year: '1993',
     description: 'FPS revolucionário onde você batalha contra demônios do Inferno como um fuzileiro espacial em Marte.',
-    imageUrl: 'src/assets/images/doom.png',
+    imageUrl: 'images/doom.png',
     archiveUrl: 'https://archive.org/details/DoomsharewareEpisode',
   },
   {
@@ -47,7 +47,7 @@ export const games: Game[] = [
     title: 'Speed Racer in The Challenge of Racer X',
     year: '1992',
     description: 'Jogo de corrida clássico com personagem carismático',
-    imageUrl: 'src/assets/images/sr.png',
+    imageUrl: 'images/sr.png',
     archiveUrl: 'https://archive.org/details/msdos_Speed_Racer_in_The_Challenge_of_Racer_X_1992',
   }
 ]; 
